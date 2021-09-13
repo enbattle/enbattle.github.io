ff95d3e87680a7d971c677b185af4fc6d8c5b44b
Steven Li <stevenli44@hotmail.com>
2021-09-12 21:35:33 -0400

    Changed the css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './App.css';
 
 import {
   HashRouter,
