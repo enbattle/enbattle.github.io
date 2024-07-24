8bbffe3ab3a3588a3451fc5d6ee79c288c7e7fba
Steven Li <listeven6691@gmail.com>
2024-07-24 13:30:52 -0400

    Adding next configurations for prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   compiler: {
     styledComponents: true,
   },
   reactStrictMode: true,
-  output: "export",
-  basePath: "/enbattle.github.io",
-  assetPrefix: "/enbattle.github.io/",
+  output: isProd ? "export" : undefined,
+  basePath: isProd ? "/<repository-name>" : "",
+  assetPrefix: isProd ? "/<repository-name>/" : "",
   trailingSlash: true,
 };
 
