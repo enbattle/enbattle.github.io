410f943f2fa5ccef9b9c47ac25d5ff5453bc1ec2
Steven Li <listeven6691@gmail.com>
2024-07-24 13:53:16 -0400

    Finalized next.js configurations for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   compiler: {
     styledComponents: true,
   },
   reactStrictMode: true,
-  output: isProd ? "export" : undefined,
-  basePath: isProd ? "/my-website" : "",
-  assetPrefix: isProd ? "/my-website/" : "",
+  // output: isProd ? "export" : undefined,
+  // basePath: isProd ? "/my-website" : "",
+  // assetPrefix: isProd ? "/my-website/" : "",
+  basePath: "",
+  assetPrefix: "",
+  output: "export",
   trailingSlash: true,
 };
 
